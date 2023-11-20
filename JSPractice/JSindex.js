21fca30c9e7b104a2236d05d084e19959220081a
adelewatkins <adele_watkins@hotmail.com>
2023-11-20 12:00:41 +0000

    Init JS task

---
+++ JSPractice/JSindex.js
@@ -0,0 +1,25 @@
+let age = 27
+
+console.log(age)
+
+let myname = "Adele"
+
+console.log(myname)
+
+let dogs = false
+
+console.log(dogs)
+
+let demo;
+
+console.log(demo)
+
+demo=null
+
+console.log(demo)
+
+console.log(typeof myname)
+
+age = "twenty seven"
+
+console.log(age)
