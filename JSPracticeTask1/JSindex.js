3e9340247103264015dca4e05c559cf1e8212961
adelewatkins <adele_watkins@hotmail.com>
2023-11-20 15:10:17 +0000

    Task 2 completed

---
+++ JSPracticeTask1/JSindex.js
@@ -0,0 +1,33 @@
+let age = 27;
+
+console.log(age);
+
+let myname = "Adele";
+
+console.log(myname);
+
+let dogs = false;
+
+console.log(dogs);
+
+let demo;
+
+console.log(demo);
+
+demo=null;
+
+console.log(demo);
+
+console.log(typeof myname);
+
+age = "twenty seven";
+
+console.log(age);
+//const is a constant
+const surname = "Watkins";
+
+console.log(surname);
+
+//Cant change a constant
+
+surname = "Hicks";
