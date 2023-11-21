677717cbbdc01c6f098f47df16623c66b993623e
adelewatkins <adele_watkins@hotmail.com>
2023-11-21 14:59:28 +0000

    loops

--- FizzBuzzTask/index.js
+++ FizzBuzzTask/index.js
@@ -1,9 +1,13 @@
-let num = 15
+// FizzBuzz, running numbers 1-100 through it
 
-if (num % 3 == 0 && num % 5 == 0) {
-    console.log("FizzBuzz")
-} else if (num % 3 == 0) {
-    console.log("Fizz")
-} else if (num % 5 == 0) {
-    console.log("Buzz")
+for (let num = 1; num <= 100; num++) {
+
+    if (num % 3 == 0 && num % 5 == 0) {
+        console.log("FizzBuzz")
+    } else if (num % 3 == 0) {
+        console.log("Fizz")
+    } else if (num % 5 == 0) {
+        console.log("Buzz")
+    }
+    else console.log(num)
 }
---
+++ index.js
@@ -0,0 +1,6 @@
+// This will print hello 10x
+
+for (let i = 1; i <= 10; i++) {
+    console.log("Hello")
+}
+
