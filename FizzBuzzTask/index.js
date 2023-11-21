e61a1e596255379b1f20aeb931c6d0458f144ef8
adelewatkins <adele_watkins@hotmail.com>
2023-11-21 12:52:07 +0000

    conditionals fizz buzz

---
+++ FizzBuzzTask/index.js
@@ -0,0 +1,9 @@
+let num = 5
+
+if (num % 3 == 0 && num % 5 == 0){
+    console.log("FizzBuzz")
+} else if (num % 3 == 0) {
+    console.log("Fizz")
+} else if (num % 5 == 0) {
+    console.log ("Buzz")
+}
