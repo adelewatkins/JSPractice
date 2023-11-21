080ccc534cdd7a7087e680f98ad7632042edbb46
adelewatkins <adele_watkins@hotmail.com>
2023-11-21 13:41:30 +0000

    formatted

--- FizzBuzzTask/index.js
+++ FizzBuzzTask/index.js
@@ -1,9 +1,9 @@
-let num = 5
+let num = 15
 
-if (num % 3 == 0 && num % 5 == 0){
+if (num % 3 == 0 && num % 5 == 0) {
     console.log("FizzBuzz")
 } else if (num % 3 == 0) {
     console.log("Fizz")
 } else if (num % 5 == 0) {
-    console.log ("Buzz")
+    console.log("Buzz")
 }
