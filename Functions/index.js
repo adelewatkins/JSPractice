6f25138e979807531369f3a30be131ab00f470eb
adelewatkins <adele_watkins@hotmail.com>
2023-11-22 16:15:33 +0000

    basic math functions

---
+++ Functions/index.js
@@ -0,0 +1,18 @@
+// function to do addition
+
+function sum(num, num2) {
+    console.log('Sum', num + num2);
+}
+sum(12, 100)
+
+//  function to do a multiplication
+function multiply(num, num2) {
+    console.log('Multiply', num * num2);
+}
+multiply(5, 5)
+
+// function to do a division
+function divide(num, num2) {
+    console.log('Division', num / num2);
+}
+divide(30, 2)
