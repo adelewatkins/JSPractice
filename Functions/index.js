b57c07122c0e3d20fe983524392cdbd0eec938fe
adelewatkins <adele_watkins@hotmail.com>
2023-11-23 11:05:21 +0000

    return function first tasks

--- Functions/index.js
+++ Functions/index.js
@@ -1,6 +1,6 @@
 // function to do addition with a log
 function sum(num, num2) {
-    console.log('Sum',num + num2);
+    console.log('Sum', num + num2);
 }
 // This then provides the variables for the function
 // and they get logged
@@ -25,7 +25,7 @@ console.log(divide(30, 2))
 
 
 // FizzBuzz with a return rather than a log
-function FizzBuzz(numb){
+function FizzBuzz(numb) {
     if (numb % 3 == 0 && numb % 5 == 0) {
         return "FizzBuzz";
     } else if (numb % 3 == 0) {
@@ -34,4 +34,20 @@ function FizzBuzz(numb){
         return "Buzz";
     }
     else return numb;
+}
+
+
+// ext task
+function largest(fig1, fig2) {
+    if (fig1 > fig2) {
+        return fig1
+    } else return fig2
+}
+
+function smallest(fig1, fig2, fig3) {
+    if (fig1 < fig2 && fig1 < fig3) {
+        return fig1;
+    } else if (fig2 < fig1 && fig2 < fig3) {
+        return fig2;
+    } else return fig3;
 }
